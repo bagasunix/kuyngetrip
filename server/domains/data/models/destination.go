eb02a239f64ad30454b76f45c2c370c7c3093d10
Aldino Pratama <aldinopratama15@gmail.com>
2023-05-29 23:29:39 +0700

    create tour destination model

---
+++ server/domains/data/models/destination.go
@@ -0,0 +1,78 @@
+package models
+
+import "github.com/gofrs/uuid"
+
+type Destination struct {
+	BaseModel
+	TourID                 uuid.UUID `gorm:"not null;type:uuid;"`
+	Tour                   *Tour     `gorm:"foreignKey:TourID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	DestinationName        string
+	DestinationDescription string
+	Country
+	Province
+	City
+	SubDistrict
+	Village
+	Coordinate
+	ImageDestination []TourImage
+}
+
+// Builder Object for Destination
+type DestinationBuilder struct {
+	BaseModelBuilder
+	tourID uuid.UUID
+	tour   *Tour
+	CountryBuilder
+	ProvinceBuilder
+	CityBuilder
+	SubDistrictBuilder
+	VillageBuilder
+	CoordinateBuilder
+	destinationName        string
+	destinationDescription string
+	imageDestination       []TourImage
+}
+
+// Constructor for DestinationBuilder
+func NewDestinationBuilder() *DestinationBuilder {
+	o := new(DestinationBuilder)
+	return o
+}
+
+// Build Method which creates Destination
+func (b *DestinationBuilder) Build() *Destination {
+	o := new(Destination)
+	o.BaseModel = *b.BaseModelBuilder.Build()
+	o.TourID = b.tourID
+	o.Tour = b.tour
+	o.Country = *b.CountryBuilder.Build()
+	o.Province = *b.ProvinceBuilder.Build()
+	o.City = *b.CityBuilder.Build()
+	o.SubDistrict = *b.SubDistrictBuilder.Build()
+	o.Village = *b.VillageBuilder.Build()
+	o.DestinationName = b.destinationName
+	o.DestinationDescription = b.destinationDescription
+	o.ImageDestination = b.imageDestination
+	return o
+}
+
+// Setter method for the field tour of type *Tour in the object DestinationBuilder
+func (d *DestinationBuilder) SetTour(tour *Tour) {
+	d.tour = tour
+	d.tourID = tour.ID
+}
+
+// Setter method for the field destinationName of type string in the object DestinationBuilder
+func (d *DestinationBuilder) SetDestinationName(destinationName string) {
+	d.destinationName = destinationName
+}
+
+// Setter method for the field destinationDescription of type string in the object DestinationBuilder
+func (d *DestinationBuilder) SetDestinationDescription(destinationDescription string) {
+	d.destinationDescription = destinationDescription
+}
+
+// Setter method for the field imageDestination of type []TourImage in the object DestinationBuilder
+func (d *DestinationBuilder) SetImageDestination(imageDestination []TourImage) {
+	d.imageDestination = imageDestination
+}
