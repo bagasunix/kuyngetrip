568f1789e14271b9d49faf61bfdc091b4063086a
Aldino Pratama <aldinopratama15@gmail.com>
2023-06-08 22:00:57 +0700

    create domain service

---
+++ server/domains/service.go
@@ -0,0 +1,53 @@
+package domains
+
+import (
+	"github.com/go-redis/redis/v8"
+	"go.uber.org/zap"
+
+	"github.com/bagasunix/kuyngetrip/server/domains/data/repositories"
+)
+
+type Service interface {
+}
+
+type service struct {
+}
+
+// Builder Object for service
+type serviceBuilder struct {
+	logger       *zap.Logger
+	jwtKey       string
+	repositories repositories.Repositories
+	middlewares  []Middleware
+	redis        *redis.Client
+}
+
+// Constructor for serviceBuilder
+func NewserviceBuilder(logs *zap.Logger, jwtKey string, repositories repositories.Repositories, redis *redis.Client) *serviceBuilder {
+	o := new(serviceBuilder)
+	o.logger = logs
+	o.jwtKey = jwtKey
+	o.repositories = repositories
+	o.redis = redis
+	return o
+}
+
+func buildService(logs *zap.Logger, jwtKey string, repositories repositories.Repositories, redis *redis.Client) Service {
+	svc := new(service)
+	return svc
+}
+
+// Build Method which creates service
+func (b *serviceBuilder) Build() Service {
+	svc := buildService(b.logger, b.jwtKey, b.repositories, b.redis)
+	for _, m := range b.middlewares {
+		svc = m(svc)
+	}
+	return svc
+}
+
+// Setter method for the field middlewares of type []Middleware in the object serviceBuilder
+func (s *serviceBuilder) SetMiddlewares(middlewares []Middleware) *serviceBuilder {
+	s.middlewares = middlewares
+	return s
+}
