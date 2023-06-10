4bda211c2801da79faf1fcfec924d0e7742f1ca6
Aldino Pratama <aldinopratama15@gmail.com>
2023-06-10 22:20:00 +0700

    add logging crete user

--- server/domains/middlewares/logging.go
+++ server/domains/middlewares/logging.go
@@ -1,14 +1,27 @@
 package middlewares
 
 import (
+	"context"
+	"time"
+
 	"go.uber.org/zap"
 
 	"github.com/bagasunix/kuyngetrip/server/domains"
+	"github.com/bagasunix/kuyngetrip/server/endpoints/requests"
+	"github.com/bagasunix/kuyngetrip/server/endpoints/responses"
 )
 
 type loggingMiddleware struct {
 	logs *zap.Logger
 	next domains.Service
+}
+
+// CreateUser implements domains.Service.
+func (l *loggingMiddleware) CreateUser(ctx context.Context, request *requests.CreateUser) (response *responses.EntityId, errs error) {
+	defer func(begin time.Time) {
+		l.logs.Info("CreateUser", zap.String("request", string(request.ToJSON())), zap.String("response", string(response.ToJSON())), zap.Error(errs), zap.Duration("took", time.Since(begin)))
+	}(time.Now())
+	return l.next.CreateUser(ctx, request)
 }
 
 func LoggingMiddleware(logs *zap.Logger) domains.Middleware {
