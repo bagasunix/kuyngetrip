9b0c70e030d2aa05940960ff935a7d963212844b
Aldino Pratama <aldinopratama15@gmail.com>
2023-07-29 13:18:08 +0700

    upload config db

---
+++ pkg/db/db.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"go.uber.org/zap"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/plugin/dbresolver"
+
+	"bagasunix/kuyngetrip/pkg/errors"
+)
+
+func NewDB(ctx context.Context, logger *zap.Logger, dbConfig *DbConfig) *gorm.DB {
+	db, err := gorm.Open(postgres.Open(dbConfig.GetDSN()), &gorm.Config{SkipDefaultTransaction: true, PrepareStmt: true})
+	errors.HandlerWithOSExit(logger, err, "init", "database", "config", dbConfig.GetDSN())
+	errors.HandlerWithOSExit(logger, db.WithContext(ctx).Use(dbresolver.Register(dbresolver.Config{}).SetMaxIdleConns(dbConfig.MaxIdleConns).SetMaxOpenConns(dbConfig.MaxOpenConns).SetConnMaxLifetime(time.Hour)), "db_resolver")
+	return db
+}
