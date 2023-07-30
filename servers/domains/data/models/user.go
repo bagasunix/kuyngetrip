44a10c1cbc13b328f64e758e693bdb5530458021
Aldino Pratama <aldinopratama15@gmail.com>
2023-07-30 16:20:14 +0700

    update user model

--- servers/domains/data/models/user.go
+++ servers/domains/data/models/user.go
@@ -1,26 +1,40 @@
 package models
 
+import "time"
+
 type User struct {
 	BaseModel
-	UserName   string
-	Email      string `gorm:"size:100;uniqueIndex:idx_user_email_unique"`
-	Password   string
-	UserType   string `gorm:"size:1;comment:1 - Owner, 2 - Admin, 3 - Guide, 4 - Participant, 5 - Administrator"`
-	Picture    string
-	Active     string     `gorm:"size:1;comment:0 - NonActive, 1 - Active, 2 - Pending, 3 - Suspended"`
-	UserDetail UserDetail `gorm:"foreignKey:UserID"`
+	UserName              string `gorm:"size:100;uniqueIndex:idx_user_name_unique"`
+	Email                 string `gorm:"uniqueIndex:idx_user_email_unique"`
+	Password              string
+	UserType              string `gorm:"size:1;comment:1 - Administrator, 2 - Admin, 3 - Guide, 4 - Participant"`
+	Picture               *string
+	Active                string `gorm:"size:1;comment:0 - NonActive, 1 - Active, 2 - Pending, 3 - Suspended"`
+	LastLogin             *time.Time
+	IpLogin               *string
+	BrowserLogin          *string
+	IpChangePassword      *string
+	BrowserChangePassword *string
+	LastChangePassword    *time.Time
+	UserDetail            UserDetail `gorm:"foreignKey:UserID"`
 }
 
 // Builder Object for User
 type UserBuilder struct {
 	BaseModelBuilder
-	userName   string
-	email      string
-	password   string
-	userType   string
-	picture    string
-	activite   string
-	userDetail UserDetail
+	userName              string
+	email                 string
+	password              string
+	userType              string
+	picture               *string
+	activite              string
+	lastLogin             *time.Time
+	ipLogin               *string
+	browserLogin          *string
+	ipChangePassword      *string
+	browserChangePassword *string
+	lastChangePassword    *time.Time
+	userDetail            UserDetail
 }
 
 // Constructor for UserBuilder
@@ -38,6 +52,12 @@ func (b *UserBuilder) Build() *User {
 	o.UserType = b.userType
 	o.Picture = b.picture
 	o.Active = b.activite
+	o.LastLogin = b.lastLogin
+	o.IpLogin = b.ipLogin
+	o.BrowserLogin = b.browserLogin
+	o.IpChangePassword = b.ipChangePassword
+	o.BrowserChangePassword = b.browserChangePassword
+	o.LastChangePassword = b.lastChangePassword
 	o.UserDetail = b.userDetail
 	return o
 }
@@ -58,7 +78,7 @@ func (u *UserBuilder) SetPassword(password string) {
 }
 
 // Setter method for the field picture of type string in the object UserBuilder
-func (u *UserBuilder) SetPicture(picture string) {
+func (u *UserBuilder) SetPicture(picture *string) {
 	u.picture = picture
 }
 
@@ -75,4 +95,34 @@ func (u *UserBuilder) SetUserType(userType string) {
 // Setter method for the field activite of type string in the object UserBuilder
 func (u *UserBuilder) SetActivite(activite string) {
 	u.activite = activite
+}
+
+// Setter method for the field lastLogin of type *time.Time in the object UserBuilder
+func (u *UserBuilder) SetLastLogin(lastLogin *time.Time) {
+	u.lastLogin = lastLogin
+}
+
+// Setter method for the field ipLogin of type *string in the object UserBuilder
+func (u *UserBuilder) SetIpLogin(ipLogin *string) {
+	u.ipLogin = ipLogin
+}
+
+// Setter method for the field browserLogin of type *string in the object UserBuilder
+func (u *UserBuilder) SetBrowserLogin(browserLogin *string) {
+	u.browserLogin = browserLogin
+}
+
+// Setter method for the field ipChangePassword of type *string in the object UserBuilder
+func (u *UserBuilder) SetIpChangePassword(ipChangePassword *string) {
+	u.ipChangePassword = ipChangePassword
+}
+
+// Setter method for the field lastChangePassword of type *time.Time in the object UserBuilder
+func (u *UserBuilder) SetLastChangePassword(lastChangePassword *time.Time) {
+	u.lastChangePassword = lastChangePassword
+}
+
+// Setter method for the field browserChangePassword of type *string in the object UserBuilder
+func (u *UserBuilder) SetBrowserChangePassword(browserChangePassword *string) {
+	u.browserChangePassword = browserChangePassword
 }
